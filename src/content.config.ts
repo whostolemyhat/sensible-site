7121d7f524481ab5fe428b9649dc22f35cb7afd2
James Baum <james@jamesbaum.co.uk>
2026-07-09 20:28:41 +1000

    site setup

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { file, glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "zod";
+
+
+const ProjectSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  img: z.string(),
+  link: z.string(),
+  date: z.string(),
+});
+
+const projects = defineCollection({
+  loader: file("src/data/games.json"),
+  schema: ProjectSchema,
+});
+
+export const collections = { projects };
